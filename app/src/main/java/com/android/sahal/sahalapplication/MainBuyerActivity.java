30a3d63529045467549152cdbcf21aff91458005
Ahmad <sasuke242@outlook.sa>
2018-07-20 09:53:53 +0300

    Main buyer activity
    add the main buyer activity with navigations and make it the launcher 100% working

---
+++ app/src/main/java/com/android/sahal/sahalapplication/MainBuyerActivity.java
@@ -0,0 +1,75 @@
+package com.android.sahal.sahalapplication;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class MainBuyerActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.navigation_buyer_home:
+
+
+                    return true;
+                case R.id.navigation_buyer_search:
+
+
+
+                    return true;
+                case R.id.navigation_buyer_profile:
+
+
+                    //   mTextMessage.setText(R.string.title_notifications);
+                    return true;
+
+                case R.id.navigation_buyer_cart:
+
+
+                    return true;
+                case R.id.navigation_buyer_dots:
+
+
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_buyer);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        navigation.setSelectedItemId(R.id.navigation_buyer_home);
+    }
+
+}
