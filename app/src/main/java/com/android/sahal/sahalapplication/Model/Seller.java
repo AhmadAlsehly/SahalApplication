e78333c8d6874cb77f88c1e2e47ec5ac0f40a023
Ahmad <sasuke242@outlook.sa>
2018-07-25 11:09:48 +0300

    very small change
    just names

--- app/src/main/java/com/android/sahal/sahalapplication/Model/Seller.java
+++ app/src/main/java/com/android/sahal/sahalapplication/Model/Seller.java
@@ -2,14 +2,14 @@ package com.android.sahal.sahalapplication.Model;
 
 public class Seller {
     public String bR ;
-    public String company ;
-    public String  number ;
+    public String companyName ;
+    public String  mobileNumber ;
 
 
     public Seller(String bR, String company, String number) {
         this.bR = bR;
-        this.company = company;
-        this.number = number;
+        this.companyName = company;
+        this.mobileNumber = number;
 
     }
 
