b3246206a5daab182a59fdd42363903120f40b85
Ahmad <sasuke242@outlook.sa>
2018-07-25 12:08:03 +0300

    smaler changes
    dialog and names

--- app/src/main/java/com/android/sahal/sahalapplication/Buyer/Fragment/FragmentBuyerOprations.java
+++ app/src/main/java/com/android/sahal/sahalapplication/Buyer/Fragment/FragmentBuyerOprations.java
@@ -1,7 +1,11 @@
 package com.android.sahal.sahalapplication.Buyer.Fragment;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
@@ -13,22 +17,47 @@ import android.view.ViewGroup;
 
 import com.android.sahal.sahalapplication.Adapters.BuyerOprationsPager;
 import com.android.sahal.sahalapplication.Adapters.OprationsPager;
+import com.android.sahal.sahalapplication.MainFirstActivity;
 import com.android.sahal.sahalapplication.R;
 import com.android.sahal.sahalapplication.Seller.Fragment.SellerOrder;
 import com.android.sahal.sahalapplication.Seller.Fragment.SellerSoldItems;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class FragmentBuyerOprations extends Fragment implements FragmentBuyerBought.OnFragmentInteractionListener,
         FragmentBuyerOrders.OnFragmentInteractionListener {
-
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        final FirebaseUser user = mAuth.getCurrentUser();
 
+        if(user == null){
+    AlertDialog.Builder builder;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        builder = new AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Dialog_Alert);
+    } else {
+        builder = new AlertDialog.Builder(getContext());
+    }
+    builder.setTitle("سجل الدخول من فضلك")
+            .setMessage("الرجاء تسجيل الدخول لتتمكن من الشراء")
+            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // continue with delete
 
+                }
+            })
+
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .show();}
         TabLayout tabLayout = view.findViewById(R.id.tabsBuyersOP);
         tabLayout.addTab(tabLayout.newTab().setText("مشترياتي"));
         tabLayout.addTab(tabLayout.newTab().setText("طلباتي"));
